12cf7a13147efdda89973bfda0554a2839352d29
diogoqds <diogoqueirozsantos97@gmai.com>
2018-09-06 10:39:51 -0300

    register and authenticate user

--- start/routes.js
+++ start/routes.js
@@ -15,6 +15,5 @@
 
 const Route = use('Route')
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-})
+Route.post('/users', 'UserController.create')
+Route.post('/sessions', 'SessionController.create')
